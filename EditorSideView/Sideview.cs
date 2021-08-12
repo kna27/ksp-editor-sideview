dbc7fb3c5913de46f860219cef177ec10ff27e97
kna27 <krisharora27@gmail.com>
2021-08-11 20:15:04 -0400

    Add comments

--- EditorSideView/Sideview.cs
+++ EditorSideView/Sideview.cs
@@ -7,6 +7,14 @@ namespace Sideview
     {
         public static void SnapCameraAngle()
         {
+            // Note, this currently does not work!
+            // Most likely it is because one of the following components
+            // are locking the camera's rotation:
+            // VABCamera
+            // SPHCamera
+            // CameraOffCenter
+            // EditorCameraOffset
+            // EditorCamera
             var vec = Camera.main.transform.rotation.eulerAngles;
             vec.x = Mathf.Round(vec.x / 90) * 90;
             vec.y = Mathf.Round(vec.y / 90) * 90;
