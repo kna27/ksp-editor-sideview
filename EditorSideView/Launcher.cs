f2e8846446e1d7a760eb35084a36c59ccdc6a459
kna27 <krisharora27@gmail.com>
2021-08-11 20:08:26 -0400

    Add the logic

---
+++ EditorSideView/Launcher.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using KSP.UI.Screens;
+using System.IO;
+
+namespace Sideview
+{
+
+    using MonoBehavior = MonoBehaviour;
+
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class AppLauncher : MonoBehavior
+    {
+        /// <summary>
+        /// Toolbar button will be visible in VAB and SPH
+        /// </summary>
+        private const ApplicationLauncher.AppScenes VisibleInScenes =
+            ApplicationLauncher.AppScenes.VAB |
+            ApplicationLauncher.AppScenes.SPH;
+        /// <summary>
+        /// Path to the icon for the toolbar button
+        /// </summary>
+        private static string appIconPath = @"/GameData/sideview/icon.png";
+        private static Texture2D appIcon;
+
+        private ApplicationLauncherButton launcher;
+
+        public void Start()
+        {
+            // Get base of app icon path
+            appIconPath = Application.platform == RuntimePlatform.OSXPlayer ? Directory.GetParent(Directory.GetParent(Application.dataPath).ToString()).ToString() : Directory.GetParent(Application.dataPath).ToString();
+            appIconPath += @"/GameData/sideview/icon.png";
+            // Create a Texture2D using icon.png
+            appIcon ??= new Texture2D(32, 32);
+            appIcon.LoadImage(File.ReadAllBytes(appIconPath));
+
+            GameEvents.onGUIApplicationLauncherReady.Add(AddLauncher);
+            GameEvents.onGUIApplicationLauncherDestroyed.Add(RemoveLauncher);
+        }
+
+        public void OnDisable()
+        {
+            GameEvents.onGUIApplicationLauncherReady.Remove(AddLauncher);
+            GameEvents.onGUIApplicationLauncherDestroyed.Remove(RemoveLauncher);
+            RemoveLauncher();
+        }
+
+        public Vector3 GetAnchor()
+        {
+            return launcher?.GetAnchor() ?? Vector3.right;
+        }
+
+        private void AddLauncher()
+        {
+            if (ApplicationLauncher.Ready && launcher == null)
+            {
+                launcher = ApplicationLauncher.Instance.AddModApplication(
+                    Sideview.SnapCameraAngle, Sideview.SnapCameraAngle,
+                    null, null,
+                    null, null,
+                    VisibleInScenes, appIcon
+                );
+            }
+        }
+
+        private void RemoveLauncher()
+        {
+            if (launcher == null) return;
+            ApplicationLauncher.Instance.RemoveModApplication(launcher);
+            launcher = null;
+        }
+    }
+}
---
+++ EditorSideView/Sideview.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Sideview
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    public class Sideview : MonoBehaviour
+    {
+        public static void SnapCameraAngle()
+        {
+            var vec = Camera.main.transform.rotation.eulerAngles;
+            vec.x = Mathf.Round(vec.x / 90) * 90;
+            vec.y = Mathf.Round(vec.y / 90) * 90;
+            vec.z = Mathf.Round(vec.z / 90) * 90;
+            Camera.main.transform.eulerAngles = new Vector3(vec.x, vec.y, vec.z);
+        }
+    }
+}
